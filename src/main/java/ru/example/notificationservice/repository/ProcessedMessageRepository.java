6b5dc2abba63e13e147dfbceaf37cf24b8bcedba
fanat1kq <110695423+fanat1kq@users.noreply.github.com>
2025-11-21 12:55:17 +0300

    add env.axample

--- src/main/java/ru/example/notificationservice/repository/ProcessedMessageRepository.java
+++ src/main/java/ru/example/notificationservice/repository/ProcessedMessageRepository.java
@@ -3,8 +3,6 @@ package ru.example.notificationservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.example.notificationservice.entity.ProcessedMessage;
 
-import java.util.Optional;
-
 public interface ProcessedMessageRepository extends JpaRepository<ProcessedMessage, String> {
     void deleteByDeduplicationKey(String deduplicationKey);
 }
